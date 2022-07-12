aa5b2f4f33e665fc8cad34d818ae249e7f598149
Nat Condit-Schultz <natcs@gatech.edu>
2022-07-12 13:53:36 -0400

    Trying to get ignoring of docs workjing...

--- doc/HumdrumSyntax.R
+++
@@ -1,8 +0,0 @@
-## ---- include = FALSE----------------------------------------------------
-knitr::opts_knit$set(root.dir = humdrumR::humdrumRroot)
-
-knitr::opts_chunk$set(
-  collapse = TRUE,
-  comment = ""
-)
-
