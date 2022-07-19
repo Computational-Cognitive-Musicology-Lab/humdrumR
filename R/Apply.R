800a8648328401ae983f59e063d979beb269c792
Nat Condit-Schultz <natcs@gatech.edu>
2022-07-19 07:16:09 -0400

    Sped things up slightly by skipping use of setActiveFields entirely.

--- R/Apply.R
+++ R/Apply.R
@@ -383,7 +383,7 @@ within.humdrumR <- function(data, ..., variables = list()) {
   # tell the humdrumR object about the new fields and set the Active formula.
   if (length(newfields)) {
     addFields(humdrumR) <- newfields
-    humdrumR <- setActiveFields(humdrumR, newfields, updateNull = FALSE) 
+    humdrumR@Active <- rlang::quo(list(!!!(rlang::syms(newfields))))
   }
   humdrumR
   # update_humdrumR(humdrumR, field = c(newfields, overWrote))
