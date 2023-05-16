e385f8ee1f217b8c87f64434ffea2fc6354d24e1
Nat Condit-Schultz <natcs@gatech.edu>
2023-05-16 09:47:33 -0400

    Added basic filter() method.

--- R/Tidy.R
+++ R/Tidy.R
@@ -7,7 +7,7 @@
 ### mutate ----
 
 #' @export
-mutate.humdrumR <- function(.data, ...) {
+mutate.humdrumR <- function(.data, ..., dataTypes = 'D', alignLeft = TRUE, expandPaths = FALSE) {
   exprs <- rlang::enquos(...)
   
   names <- .names(exprs)
@@ -19,14 +19,17 @@ mutate.humdrumR <- function(.data, ...) {
                             })
   names(exprs) <- NULL
   
-  rlang::eval_tidy(rlang::quo(within.humdrumR(.data, !!!exprs)))
+  rlang::eval_tidy(rlang::quo(within.humdrumR(.data, !!!exprs, 
+                                              dataTypes = !!dataTypes,
+                                              alignLeft = !!alignLeft,
+                                              expandPaths = !!expandPaths)))
   
 }
 
 ### summarize ----
 
 #' @export
-summarize.humdrumR <- function(.data, ...) {
+summarize.humdrumR <- function(.data, ..., dataTypes = 'D', expandPaths = FALSE, drop = FALSE) {
   exprs <- rlang::enquos(...)
   
   names <- .names(exprs)
@@ -38,17 +41,37 @@ summarize.humdrumR <- function(.data, ...) {
                             })
   names(exprs) <- NULL
   
-  rlang::eval_tidy(rlang::quo(with.humdrumR(.data, !!!exprs, drop = FALSE)))
+  rlang::eval_tidy(rlang::quo(with.humdrumR(.data, !!!exprs, 
+                                            dataTypes = !!dataTypes,
+                                            drop = !!drop)))
   
 }
 
 ### pull ----
 
 #' @export
-pull.humdrumR <- function(.data, var, ...) {
+pull.humdrumR <- function(.data, var, ..., dataTypes = 'D') {
   field <- rlang::enquo(var)
   
   
-  rlang::eval_tidy(rlang::quo(with.humdrumR(.data, !!field, ...)))
+  rlang::eval_tidy(rlang::quo(with.humdrumR(.data, !!field, dataTypes = !!dataTypes)))
   
+}
+
+
+### filter ----
+#' @export
+filter.humdrumR <- function(.data, ...) {
+  exprs <- rlang::enquos(...)
+  
+  names <- .names(exprs)
+  exprs[names != ''] <- Map(exprs[names != ''], names[names != ''],
+                            f = \(expr, name) {
+                              name <- rlang::sym(name)
+                              
+                              rlang::quo(!!name <- !!expr)
+                            })
+  names(exprs) <- NULL
+  
+  rlang::eval_tidy(rlang::quo(subset.humdrumR(.data, !!!exprs))) 
 }
